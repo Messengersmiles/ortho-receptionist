720d9989149ad0f0db980ae384fc35b924841e49
Messengersmiles <doc@messenger-smiles.com>
2026-06-25 15:51:24 -0700

    Add website closing message after schedule/reschedule times collected

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ function getTextHeader(typeLabel) {
     case "Schedule":
       return "📅 Schedule Appointment";
     case "Reschedule":
-      return "➡️ Reschedule";
+      return "Reschedule";
     case "Comfort Visit":
       return "❗ Comfort Visit";
     case "Question":
